30949dde75f310e8cdc479dc5a66f246ce4975d4
Dan Charvat <dan@elision.design>
2019-05-02 11:57:35 +0200

    Configure debugger.

--- test/fixtures/nuxt.config.ts
+++ test/fixtures/nuxt.config.ts
@@ -1,47 +1,54 @@
 const modulePath = require('../../src/module')
 
 export default {
-  env: {},
-  srcDir: __dirname,
-  dev: false,
-  render: {
-    resourceHints: false
-  },
-  modules: [
-    '@nuxtjs/axios',
-    [modulePath, {
-      seo: true,
-      baseUrl: 'nuxt-app.localhost',
-      locales: [
-        {
-          code: 'en',
-          iso: 'en-US',
-          name: 'English'
-        },
-        {
-          code: 'fr',
-          iso: 'fr-FR',
-          name: 'Français'
+    env: {},
+    srcDir: __dirname,
+    dev: false,
+    render: {
+        resourceHints: false
+    },
+    modules: [
+        '@nuxtjs/axios',
+        [modulePath, {
+            seo: true,
+            baseUrl: 'nuxt-app.localhost',
+            locales: [
+                {
+                    code: 'en',
+                    iso: 'en-US',
+                    name: 'English'
+                },
+                {
+                    code: 'fr',
+                    iso: 'fr-FR',
+                    name: 'Français'
+                }
+            ],
+            defaultLocale: 'en',
+            lazy: false,
+            vueI18n: {
+                messages: {
+                    fr: {
+                        home: 'Accueil',
+                        about: 'À propos',
+                        posts: 'Articles'
+                    },
+                    en: {
+                        home: 'Homepage',
+                        about: 'About us',
+                        posts: 'Posts'
+                    }
+                },
+                fallbackLocale: 'en'
+            }
+        }]
+    ],
+    axios: {},
+    extend(config, ctx) {
+        if (ctx.isDev) {
+            config.devtool = 'eval-source-map'
         }
-      ],
-      defaultLocale: 'en',
-      lazy: false,
-      vueI18n: {
-        messages: {
-          fr: {
-            home: 'Accueil',
-            about: 'À propos',
-            posts: 'Articles'
-          },
-          en: {
-            home: 'Homepage',
-            about: 'About us',
-            posts: 'Posts'
-          }
-        },
-        fallbackLocale: 'en'
-      }
-    }]
-  ],
-  axios: {}
+        config.devtool = 'eval'
+    // config.devtool = ctx.isDev ? 'eval-source-map' : 'inline-source-map'
+    }
 }
